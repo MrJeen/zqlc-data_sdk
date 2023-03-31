3be77c5a7bec71769dc840826851a5e6cbeb50a1
d.jy <jy20220520@gmail.com>
2023-03-31 19:41:36 +0800

    优化

--- src/entity/transfer.sync.entity.ts
+++ src/entity/transfer.sync.entity.ts
@@ -1,24 +1,9 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  Index,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Column, Entity, Index } from 'typeorm';
+import { CommonEntity } from './common.entity';
 
 @Entity('transfer_sync')
 @Index(['chain'], { unique: true })
-export class TransferSync {
-  @PrimaryGeneratedColumn({ comment: 'ID' })
-  id: number;
-
-  @CreateDateColumn({ comment: '创建时间' })
-  created_at;
-
-  @UpdateDateColumn({ comment: '更新时间' })
-  updated_at;
-
+export class TransferSync extends CommonEntity {
   @Column('varchar', { default: '', comment: '区块链类型' })
   chain;
 
