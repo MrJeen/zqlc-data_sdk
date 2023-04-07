82b44ab22425687682d11c0d4ed6bd5c35be9cd5
d.jy <jy20220520@gmail.com>
2023-04-07 18:05:55 +0800

    优化

--- src/service/contract.service.ts
+++ src/service/contract.service.ts
@@ -4,58 +4,76 @@ import {
   RABBITMQ_SYNC_NFT_EXCHANGE,
   getContractSyncSuccessSourceKey,
 } from '../config/constant';
-import { CONTRACT_SOURCE } from '../entity/contract.entity';
+import { CONTRACT_SOURCE, Contract } from '../entity/contract.entity';
 import { mqPublish } from '../utils/rabbitMQ';
 import auth from '../config/auth.api';
 import { DataSource } from 'typeorm';
-import { ContractSync } from 'entity';
+import { ContractSync } from '../entity/contract.sync.entity';
 
 export async function contractSyncNotice(
   amqpConnection: any,
   redisService: any,
   datasource: DataSource,
-  data: any,
+  contract: Contract,
 ) {
-  const contract = data.contract;
-  // 同步成功，通知三方
-  if (data.source !== CONTRACT_SOURCE.DEFAULT) {
-    const publishData = {
-      chain_id: contract.chain_id,
-      chain: contract.chain,
-      token_address: contract.token_address,
-      contract_type: contract.contract_type,
-      name: contract.name,
-      creator: contract.creator,
-      is_recommend: data.is_recommend,
-    };
+  // 查询当前系列ID，未通知的三方
+  const sourceData = await datasource.getRepository(ContractSync).findBy({
+    sync_status: COMMON_STATUS.DEFAULT,
+    chain: contract.chain,
+    token_address: contract.token_address,
+  });
 
-    // rmq推送
-    push(amqpConnection, datasource, redisService, data.source, publishData);
+  if (!sourceData.length) {
+    return;
+  }
 
-    // openmeta导入，同步推送到sudo
-    if (data.source === CONTRACT_SOURCE.OPEN_META) {
+  for (const sourceItem of sourceData) {
+    // 同步成功，通知三方
+    if (sourceItem.source !== CONTRACT_SOURCE.DEFAULT) {
+      const publishData = {
+        chain_id: contract.chain_id,
+        chain: contract.chain,
+        token_address: contract.token_address,
+        contract_type: contract.contract_type,
+        name: contract.name,
+        creator: contract.creator,
+        is_recommend: sourceItem.is_recommend,
+      };
+
+      // rmq推送
       push(
         amqpConnection,
         datasource,
         redisService,
-        CONTRACT_SOURCE.SUDO,
+        sourceItem.source,
         publishData,
       );
+
+      // openmeta导入，同步推送到sudo
+      if (sourceItem.source === CONTRACT_SOURCE.OPEN_META) {
+        push(
+          amqpConnection,
+          datasource,
+          redisService,
+          CONTRACT_SOURCE.SUDO,
+          publishData,
+        );
+      }
+
+      // 记录到缓存
+      const key = getContractSyncSuccessSourceKey(
+        contract.chain,
+        contract.token_address,
+      );
+      const redisClient = redisService.getClient();
+      await redisClient.sadd(key, sourceItem.source);
     }
 
-    // 记录到缓存
-    const key = getContractSyncSuccessSourceKey(
-      contract.chain,
-      contract.token_address,
-    );
-    const redisClient = redisService.getClient();
-    await redisClient.sadd(key, data.source);
+    // 修改同步状态
+    await datasource.getRepository(ContractSync).update(sourceItem.id, {
+      sync_status: COMMON_STATUS.FINISHED,
+    });
   }
-
-  // 修改同步状态
-  await datasource.getRepository(ContractSync).update(data.id, {
-    sync_status: COMMON_STATUS.FINISHED,
-  });
 }
 
 // rmq推送
