9dba5690de44acbca0ca97ae566f2416baf5b550
d.jy <jy20220520@gmail.com>
2023-10-16 10:50:55 +0800

    update

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -483,7 +483,7 @@ export function getTableSuffix(address: string, chunk = 200) {
 export function checkRpcError(error: any, chainId: number, node: string) {
   const errors = ['NETWORK_ERROR', 'SERVER_ERROR', 'TIMEOUT'];
   const data = {
-    chainId,
+    chain_id: chainId,
     node,
     message: error + '',
   };
